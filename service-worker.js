ef47b072c2f49f0985eb33a689cbd4d36847742f
desktop-wsl <877624892@qq.com>
2021-11-11 20:03:26 +0800

    deploy(page): version 11/11/2021, 8:03:26 PM

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "precache-manifest.3cf12011e09c7eca7bc68fae109d4642.js"
+  "precache-manifest.67670064296cb820081334796604a7ae.js"
 );
 
 workbox.core.setCacheNameDetails({prefix: "real-qingshui-pass"});
